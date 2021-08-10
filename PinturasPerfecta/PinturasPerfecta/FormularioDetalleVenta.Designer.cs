fb72f09d40c0b3d83f6787dfb6882555631b0d3d
Edgar Andrés <76711142+MassterProg@users.noreply.github.com>
2021-08-09 23:48:09 -0500

    agregando mamadas que hizo el visual pero todo ok

--- PinturasPerfecta/PinturasPerfecta/FormularioDetalleVenta.Designer.cs
+++ PinturasPerfecta/PinturasPerfecta/FormularioDetalleVenta.Designer.cs
@@ -29,12 +29,167 @@ namespace PinturasPerfecta
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelNav = new System.Windows.Forms.Panel();
+            this.buttonClose = new FontAwesome.Sharp.IconButton();
+            this.comboBoxCliente = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxIDV = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonAgregar = new FontAwesome.Sharp.IconButton();
+            this.panelNav.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelNav
+            // 
+            this.panelNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(49)))), ((int)(((byte)(63)))));
+            this.panelNav.Controls.Add(this.buttonClose);
+            this.panelNav.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelNav.Location = new System.Drawing.Point(0, 0);
+            this.panelNav.Name = "panelNav";
+            this.panelNav.Size = new System.Drawing.Size(523, 31);
+            this.panelNav.TabIndex = 50;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(74)))), ((int)(((byte)(99)))));
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            this.buttonClose.IconColor = System.Drawing.Color.White;
+            this.buttonClose.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.buttonClose.IconSize = 25;
+            this.buttonClose.Location = new System.Drawing.Point(496, 0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(26, 25);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxCliente
+            // 
+            this.comboBoxCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(57)))), ((int)(((byte)(79)))));
+            this.comboBoxCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxCliente.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.comboBoxCliente.ForeColor = System.Drawing.Color.White;
+            this.comboBoxCliente.FormattingEnabled = true;
+            this.comboBoxCliente.Location = new System.Drawing.Point(16, 89);
+            this.comboBoxCliente.Name = "comboBoxCliente";
+            this.comboBoxCliente.Size = new System.Drawing.Size(145, 28);
+            this.comboBoxCliente.TabIndex = 51;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 24);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Productos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(202, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Precio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(362, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Cantidad";
+            // 
+            // boxIDV
+            // 
+            this.boxIDV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(57)))), ((int)(((byte)(79)))));
+            this.boxIDV.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boxIDV.ForeColor = System.Drawing.Color.White;
+            this.boxIDV.Location = new System.Drawing.Point(206, 89);
+            this.boxIDV.Multiline = true;
+            this.boxIDV.Name = "boxIDV";
+            this.boxIDV.Size = new System.Drawing.Size(107, 30);
+            this.boxIDV.TabIndex = 57;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(57)))), ((int)(((byte)(79)))));
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(366, 89);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 30);
+            this.textBox1.TabIndex = 58;
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.FlatAppearance.BorderColor = System.Drawing.Color.Cyan;
+            this.buttonAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.buttonAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAgregar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAgregar.ForeColor = System.Drawing.Color.Cyan;
+            this.buttonAgregar.IconChar = FontAwesome.Sharp.IconChar.Check;
+            this.buttonAgregar.IconColor = System.Drawing.Color.Cyan;
+            this.buttonAgregar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.buttonAgregar.IconSize = 32;
+            this.buttonAgregar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonAgregar.Location = new System.Drawing.Point(197, 165);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(134, 34);
+            this.buttonAgregar.TabIndex = 59;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            // 
+            // FormularioDetalleVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(57)))), ((int)(((byte)(79)))));
+            this.ClientSize = new System.Drawing.Size(523, 211);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.boxIDV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxCliente);
+            this.Controls.Add(this.panelNav);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormularioDetalleVenta";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormularioDetalleVenta";
+            this.panelNav.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        public System.Windows.Forms.Panel panelNav;
+        private FontAwesome.Sharp.IconButton buttonClose;
+        public System.Windows.Forms.ComboBox comboBoxCliente;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox boxIDV;
+        public System.Windows.Forms.TextBox textBox1;
+        public FontAwesome.Sharp.IconButton buttonAgregar;
     }
 }
